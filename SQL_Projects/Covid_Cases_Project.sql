9b10d796bc55016df30c677d73a4698ac8b9269d
Tobicc96 <110782747+Tobicc96@users.noreply.github.com>
2022-10-08 21:51:49 -0300

    Update Covid_Cases_Project.sql

--- SQL_Projects/Covid_Cases_Project.sql
+++ SQL_Projects/Covid_Cases_Project.sql
@@ -1,7 +1,11 @@
+ -- Data Exploration 
+ 
  SELECT * 
  FROM PortfolioProject..CovidDeaths
  ORDER BY 3,4 
 
+ -- Data Exploration 
+ 
  SELECT * 
  FROM PortfolioProject..CovidVaccinations
  ORDER BY 3,4 
@@ -10,20 +14,12 @@ SELECT location, date, total_cases, new_cases, total_deaths, population
 FROM PortfolioProject..CovidDeaths
 ORDER BY 1,2
 
--- Looking at total_deaths vs total_cases
--- Shows Likelihood of dying if you contract covid in your country
-
 SELECT location, date, total_cases, total_deaths, (total_deaths/total_cases)*100 AS DeathPercentage
 FROM PortfolioProject..CovidDeaths
-WHERE location like '%states%'
 ORDER BY 1,2
-
--- Looking at total_deaths vs Population
--- Shows what percentage of population got Covid
 
 SELECT location, date, total_cases, Population, (total_cases/population)*100 AS PercentPopulationInfected
 FROM PortfolioProject..CovidDeaths
-WHERE location like '%states%'
 ORDER BY 1,2
 
 -- Looking at Countries with Highest Infection Rate compared to Population
@@ -43,9 +39,6 @@ WHERE continent IS NOT NULL
 GROUP BY Location, Population
 ORDER BY TotalDeathCount DESC
 
--- Break down by Continent
-
-
 -- Showing continents with the highest deathcounts
 
 SELECT Continent, MAX(CAST(total_deaths AS int)) AS TotalDeathCount
@@ -55,7 +48,7 @@ WHERE continent IS NOT NULL
 GROUP BY continent
 ORDER BY TotalDeathCount DESC
 
--- GLOBAL NUMBERS
+-- Global data
 
 SELECT  
 	SUM(new_cases) AS total_cases, 
@@ -68,6 +61,7 @@ WHERE continent is not null
 ORDER BY 1,2
 
 -- Looking at Total Population vs Vaccinations
+
 SELECT dea.continent, dea.location, dea.date, dea.population, vac.new_vaccinations
 , SUM(CAST(vac.new_vaccinations AS int)) OVER (PARTITION BY dea.location ORDER BY dea.location, dea.date) AS RollingPeopleVaccinated
 FROM PortfolioProject..CovidDeaths dea
@@ -77,7 +71,7 @@ JOIN PortfolioProject..CovidVaccinations vac
 WHERE dea.continent IS NOT NULL
 ORDER BY 2,3
 
--- USE CTE
+-- CTE
 
 WITH PopvsVac (Continent, Location, Date, Population, New_Vaccinations, RollingPeopleVaccinated) AS 	
 (
@@ -94,7 +88,7 @@ WHERE dea.continent IS NOT NULL
 SELECT *, (RollingPeopleVaccinated/Population)*100 AS PercentagePeopleVaccinated
 FROM PopvsVac
 
---TEMP TABLE
+--Temporary Table
 
 DROP Table if exists #PercentagePopulationVaccinated
 Create Table #PercentagePopulationVaccinated
